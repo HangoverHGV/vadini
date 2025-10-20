30b07525360e1dc6777870ac94ebb244a730ad4b
HangoverHGV <hangobogdan@gmail.com>
2025-10-20 14:43:12 +0300

    Better seo

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,16 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navabar from './Navbar';
 import ContactFooter from './ContactFooter';
 
 
 export default function Layout({ switchLocale, locale }) {
+    useEffect(() => {
+        document.documentElement.lang = locale;
+    }, [locale]);
+
     return (
         <>
-        <html lang={locale} />
             <div>
                 <Navabar switchLocale={switchLocale} locale={locale} />
                 <Outlet />
