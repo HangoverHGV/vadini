ca7dc3d19a24eb8d8bc22c171e565d1aa81b0f4d
HangoverHGV <hangobogdan@gmail.com>
2026-01-08 17:10:41 +0200

    Styling

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import Navabar from './Navbar';
-import ContactFooter from './ContactFooter';
-
+import { Outlet } from "react-router-dom";
+import Navabar from "./Navbar";
+import ContactFooter from "./ContactFooter";
 
 export default function Layout({ switchLocale, locale }) {
-    return (
-        <>
-            <div>
-                <Navabar switchLocale={switchLocale} locale={locale} />
-                <Outlet />
-                <ContactFooter />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="app-shell">
+        <Navabar switchLocale={switchLocale} locale={locale} />
+        <main className="main-content">
+          <Outlet />
+        </main>
+        <ContactFooter />
+      </div>
+    </>
+  );
 }
